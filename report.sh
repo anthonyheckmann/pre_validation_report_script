65471efe837db528c1e01c7b0848b37957e96f4c
Tony Heckmann <anthony@hyperscience.com>
2018-11-05 17:08:13 -0500

    refactor

--- report.sh
+++ report.sh
@@ -2,25 +2,51 @@
 
 
 #Sould return just version of OS . (Note \K in grep suppressed 1st group)
-OS="$( grep -oP "((?:^NAME=\")\K.*(\"))|((^VERSION=\")\K.*(\"))" /etc/os-release | tr -d \")"
+OS(){
+
+    local name_and_version_regex="((?:^NAME=\")\K.*(\"))|((^VERSION=\")\K.*(\"))"
+    grep -oP  "$name_and_version_regex" /etc/os-release | tr -d \"
+}
 
 
 #Sould return just version of Docker client. (Note \K in grep suppressed 1st group)
-DOCKER_V="$(docker version | grep -ozP  "(?s)(Client:.*?Version:\W+)\K\N*" | tr -d '\0')"
+DOCKER_V(){
+    docker version \
+    | grep -ozP  "(?s)(Client:.*?Version:\W+)\K\N*" \
+    | tr -d '\0'
+    }
 
 #prints out storage found, excluding loop and temp devices
-STORAGE_SPACE="$(df -h | grep -vE "(loop)|(tmpfs)" )"
+STORAGE_SPACE(){
+    
+    df -h | grep -vE "(loop)|(tmpfs)"
+
+} 
 
 #Looks for uid=0 via id command
-ROOT_ACCESS_INSTALL="$( if  { sudo id || id; } |  grep -q "uid=0"; then echo "Available"; else echo "No Root"; fi )"
+ROOT_ACCESS_INSTALL() {
+    if  { sudo id || id; } |  grep -q "uid=0"; then
+        echo "Available";
+    else
+        echo "No Root";
+    fi 
+}
 
 #Check for existence of sestatus command; then try running it and look if enabled
-SELINUX="$( { hash sestatus 2>/dev/null && { sestatus || sudo sestatus; } | grep -oP "(SELinux\Wstatus:\W+)\K\w*"; } \
-|| echo -e "Nothing Found" )"
+SELINUX() { 
+    if hash sestatus 2>/dev/null; then
+
+        { sestatus || sudo sestatus; } | grep -oP "(SELinux\Wstatus:\W+)\K\w*";
+    else
+        echo -e "Nothing Found"
+    fi
+}
 
 bold="\e[1m"
 reseto="\e[0m"
 dim="\e[2m"
+red="\e[31m"
+blink="\e[5m"
 
 boldo(){
 
@@ -31,26 +57,34 @@ dimmo(){
     echo -e "$dim$1$reseto"
 }
 
+blinky() {
+    echo -e "$blink$1$reseto"
+}
+
 dim_line(){
     dimmo "======================================="
 }
 
+red_red() {
+    echo -e "$red$1$reseto"
+}
+
 cat <<EOF
 $(dim_line)
-OS Version:
-$( boldo "$OS")
+Linux Distribution & Version:
+$( boldo  "$(OS)")
 $(dim_line)
 Docker Version:
-$( boldo "$DOCKER_V")
+$( boldo "$(DOCKER_V)")
 $(dim_line)
-Space Found:
-$( boldo "$STORAGE_SPACE")
+Space Report:
+$( boldo "$(STORAGE_SPACE)")
 $(dim_line)
-Root Avail?:
-$( boldo "$ROOT_ACCESS_INSTALL")
+Root Status:
+$( boldo "$(ROOT_ACCESS_INSTALL)")
 $(dim_line)
-SELinux On?:
-$( boldo "$SELINUX")
+SELinux:
+$( boldo "$(SELINUX)")
 $(dim_line)
 EOF
 
